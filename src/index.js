51aca9b1faf074d762352a5e824b53f66f312288
Test <m-mik@ro.ru>
2023-06-07 00:13:19 +0300

    -

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-export function healthStatus(name, health) {
+import './css/style.css';
 
-    if (health > 50) {
-        return 'healthy'
-    }
-    if (health <= 50 && health > 15) {
-        return 'wounded'
-    }
-    if (health <= 15) {
-        return 'critical'
-    }
-}
+import './js/app';
+
